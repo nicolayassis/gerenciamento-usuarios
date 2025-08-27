f4f4a2613c736e83e7b097bbeaf960bdaee8ede4
nicolayassis <nicolay.ac@hotmail.com>
2025-08-26 21:45:49 -0300

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,9 +5,9 @@ import { HomeComponent } from './components/home/home.component';
 
 
 export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
   { path: '', component: HomeComponent },
   { path: 'usuarios', component: ListaUsuariosComponent },
   { path: 'adicionar', component: AdicionarUsuariosComponent },
   { path: 'editar/:id', component: AdicionarUsuariosComponent },
-  { path: 'home', component: HomeComponent }
 ];
